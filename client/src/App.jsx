b8ad914db2ec1cfa036b61af1cd7ee5d28206334
Monel Lindu <monel.redlind@gmail.com>
2025-07-30 20:48:12 +0700

    Preparing pages navigation and roles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './dashboard/pages/Login';
 import MainLayout from './dashboard/layouts/MainLayout';
 import Adminindex from './dashboard/pages/Adminindex';
 import ProtectDashboard from './middleware/ProtectDashboard';
 import ProtectRole from './middleware/ProtectRole';
-import Unable from './middleware/Unable';
+import Unable from './dashboard/pages/Unable';
+import Profile from './dashboard/pages/Profile';
+import News from './dashboard/pages/News';
+import AddWriter from './dashboard/pages/AddWriter';
+import Writers from './dashboard/pages/Writers';
 
 function App() {
     
@@ -21,8 +24,13 @@ function App() {
             <Route path='' element={<Navigate to='/dashboard/admin' />} />
             <Route path='unable-access' element={<Unable />} />
 
+            <Route path='profile' element={<Profile />} />
+            <Route path='news' element={<News />} />
+
             <Route path='' element={<ProtectRole role='admin' />}>
               <Route path='admin' element={<Adminindex />} />
+              <Route path='writer/add' element={<AddWriter />} />
+              <Route path='writers' element={<Writers />} />
             </Route>
 
           </Route>
--- client/src/middleware/ProtectRole.jsx
+++ client/src/middleware/ProtectRole.jsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
-import Unable from './Unable';
 
 const ProtectRole = ({role}) => {
     const userInfo = {
