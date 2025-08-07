f194d120134ebf3d020054e93928bdd0ca7c718e
Monel Lindu <monel.redlind@gmail.com>
2025-08-07 09:16:21 +0700

    login page design

--- client/src/dashboard/pages/Login.jsx
+++ client/src/dashboard/pages/Login.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+
+    const [loader, setLoader] = useState(false);
+
     return (
-        <div>
-            <h1>Login Page</h1>
+        <div className='min-h-screen bg-slate-100 flex items-center justify-center'>
+            <div className='bg-white shadow-lg rounded-lg w-[400px]'>
+                <div className='p-8'>
+                    <div className='flex justify-center mb-8'>
+                        <img className='w-[150px]' src="https://imgur.com/7mbwgYz.jpg" alt="logo" />
+
+                    </div>
+                    <form className='space-y-6'>
+
+                        <div>
+                            <label htmlFor="email" className='block text-md font-medium text-gray-700 mb-2'>E-mail</label>
+                            <input type="email" name='email' id='email' placeholder='Enter your email' className='w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 transition' />
+                        </div>
+                        <div>
+                            <label htmlFor="password" className='block text-md font-medium text-gray-700 mb-2'>Password</label>
+                            <input type="password" name='password' id='password' placeholder='Enter your password' className='w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 transition' />
+                        </div>
+                        
+                        <div>
+                            <button type='submit' disabled={loader} className={`w-full py-3 text-white rounded-md transition font-semibold ${loader ? 'bg-blue-400 cursor-not-allowed' : 'bg-blue-600 hover:bg-blue-400'}`}>
+                                {loader ? 'Loading...' : 'Login'}
+                            </button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
