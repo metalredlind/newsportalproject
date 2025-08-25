4f405a07a4280d2d675427879a6927eda6dd5dc3
Monel Lindu <monel.redlind@gmail.com>
2025-08-25 20:51:25 +0700

    prepare model and controller for images modal

--- client/src/dashboard/pages/CreateNews.jsx
+++ client/src/dashboard/pages/CreateNews.jsx
@@ -1,4 +1,4 @@
-import { useContext, useRef, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaImages } from "react-icons/fa6";
 import JoditEditor from 'jodit-react';
@@ -54,6 +54,27 @@ const CreateNews = () => {
             toast.error(error.response.data.message);
         }
     }
+
+    const [images, setImages] = useState([]);
+    
+    const get_images = async () => {
+        try {
+            const {data} = await axios.get(`${base_url}/api/images`, {
+                headers: {
+                    'Authorization' : `Bearer ${store.token}`
+                }
+            });
+            console.log(data.images);
+            setImages(data.images);
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+    useEffect(() => {
+        get_images()
+    },[]);
+
 
     return (
         <div className='bg-white shadow-md rounded-md p-6'>
--- routes/newsRoutes.js
+++ routes/newsRoutes.js
@@ -4,5 +4,6 @@ const middleware = require('../middlewares/middleware');
 
 //writers route
 router.post('/api/news/add', middleware.auth, newsControllers.add_news);
+router.get('/api/images', middleware.auth, newsControllers.get_images);
 
 module.exports = router
