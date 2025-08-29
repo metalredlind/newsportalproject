ab423c763c86a01e0dc4996b000e19756aee0ad2
Monel Lindu <monel.redlind@gmail.com>
2025-08-29 21:44:14 +0700

    early header design

---
+++ frontend/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='bg-[#333333] text-[#cccccc]'>
+            <div className='px-5 lg:px-8 flex justify-between items-center py-2 border-b border-[#444444]'>
+
+            </div>
+
+        </header>
+    );
+};
+
+export default Header;
