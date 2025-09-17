5f5e7e097c8eb1c2697308405814d128a1bf5dd0
Monel Lindu <monel.redlind@gmail.com>
2025-09-17 09:56:46 +0700

    dynamically shows headlines

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -23,7 +23,7 @@ const Home = async() => {
   return (
     <div>
     <main>
-      <HeadLines />
+      <HeadLines news={news} />
       <div className="bg-slate-100">
         <div className="px-4 md:px-8 py-8">
           <div className="flex flex-wrap">
--- frontend/components/HeadLines.jsx
+++ frontend/components/HeadLines.jsx
@@ -3,26 +3,7 @@ import LoadingSpinnerComponent from 'react-spinners-components';
 import Marquee from 'react-fast-marquee';
 import Link from 'next/link';
 
-const HeadLines = () => {
-
-    const head = [
-        {
-            title: 'Daftar 10 Pulau Terbaik Dunia 2025: Bali Bertengger di Posisi Ketiga'
-        },
-        {
-            title: 'Rapper Amerika Melly Mike Tiba di Indonesia, Hadiri Festival Pacu Jalur'
-        },
-        {
-            title: 'Rahasia Makan Sehat Agar Awet Muda dan Panjang Umur, Terbukti dari Riset 2025'
-        },
-        {
-            title: '5 Rahasia Panjang Umur Gratis yang Jarang Orang Tahu, Nomor 3 Paling Susah Dilakukan!'
-        },
-        {
-            title: 'Profil Mauro Zijlstra dan Miliano Jonathans, Calon Pemain Timnas Indonesia'
-        }
-    ]
-
+const HeadLines = ({news}) => {
     return (
         <div className='bg-white shadow flex flex-wrap'>
             <div className='flex md:w-[170px] w-full bg-[#dddddd] relative after:absolute after:bg-[#dddddd] after:w-[20px] after:left-[160px] after:skew-x-[20deg] after:top-0 after:bottom-0 after:z-30'>
@@ -38,10 +19,14 @@ const HeadLines = () => {
                 <div className='flex w-full justify-start items-center'>
                     <Marquee>
                         {
-                            head.map((h,i) => <Link key={i} className='py-3 font-semibold hover:text-[#c80000] pr-12 text-sm' href={'/'}>
-                                {h.title}
-                            </Link>
-                            )
+                            Object.keys(news).length > 0 && 
+                            Object.keys(news).map((c,i) => <>
+                                {
+                                    news[c].length > 0 && news[c].map((n,i) => <Link key={i} className='py-3 font-semibold hover:text-[#c80000] pr-12 text-sm' href={`/news/${n.slug}`}>
+                                    {n.title}
+                                    </Link>)
+                                }
+                            </>)
                         }
                     </Marquee>
                 </div>
