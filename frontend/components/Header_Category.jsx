49eab553910088a4157fc844dc6449a57c194072
Monel Lindu <monel.redlind@gmail.com>
2025-09-17 09:48:21 +0700

    dynamically shows categories on header menu

--- frontend/components/Header_Category.jsx
+++ frontend/components/Header_Category.jsx
@@ -26,31 +26,8 @@ const Header_Category = () => {
     },[])
 
     const path = usePathname();
-    const data = [
-        {
-            id:1,
-            name: 'Sport'
-        },
-        {
-            id:2,
-            name: 'Travel'
-        },
-        {
-            id:3,
-            name: 'Education'
-        },
-        {
-            id:4,
-            name: 'National'
-        },
-        {
-            id:5,
-            name: 'Politic'
-        },
-    ];
 
-    const [cate_show, set_cate_show] = useState(false);
-
+    const [ cate_show, set_cate_show ] = useState(false);
     const [ showSearch, setShowSearch ] = useState(false);
     
     return (
@@ -65,7 +42,7 @@ const Header_Category = () => {
                     <div className='flex-wrap hidden lg:flex'>
                         <Link className={`px-6 font-medium py-[13px] ${path === '/' ? 'bg-[#00000026]' : ''} `} href={'/'} > Home </Link>
                         {
-                            data.map((c,i) => <Link key={i} className={`px-6 font-medium py-[13px] ${path === c.name ? 'bg-[#00000026]' : ''} `} href={'/'}> {c.name} </Link>
+                            categories.length > 0 && categories.map((c,i) => <Link key={i} className={`px-6 font-medium py-[13px] ${path === c.category ? 'bg-[#00000026]' : ''} `} href={`/news/category/${c.category}`} > {c.category} </Link>
                             )
                         }
                     </div>
@@ -98,9 +75,9 @@ const Header_Category = () => {
             
             {
                 cate_show && <div className='flex flex-wrap lg:hidden py-2 px-[30px]'>
-                        <Link className={`px-4 font-medium py-[5px] ${path === '/' ? 'bg-[#00000026]' : ''} `} href={'/'} > Home </Link>
+                        <Link className={`px-4 capitalize font-medium py-[5px] ${path === '/' ? 'bg-[#00000026]' : ''} `} href={'/'} > Home </Link>
                         {
-                            data.map((c,i) => <Link className={`px-4 font-medium py-[5px] ${path === c.name ? 'bg-[#00000026]' : ''} `} href={'/'}> {c.name} </Link>
+                            categories.length > 0 && categories.map((c,i) => <Link className={`px-4 capitalize font-medium py-[5px] ${path === c.category ? 'bg-[#00000026]' : ''} `} href={`/news/category/${c.category}`} > {c.category} </Link>
                             )
                         }
                     </div>
